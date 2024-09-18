6ad1219251333ca425678b9c24a930922a8eabc6
Darius K <darius.kaya@gmail.com>
2024-09-18 12:57:50 +0200

    navbar almost done

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import orto from "../../../public/images/orthodontics.png"
 import emergency from "../../../public/images/emergency.png"
 import aesthetic from "../../../public/images/aesthetic.png"
@@ -20,9 +20,22 @@ const Navbar = () => {
         setDropdownOpen(!dropdownOpen)
     }
 
+    const closeDropdown = () => {
+        setDropdownOpen(false)
+    }
+
+    useEffect(() => {
+        if (dropdownOpen) {
+            document.body.classList.add("overflow-y-hidden", "touch-none", "overscroll-none")
+        } else {
+            document.body.classList.remove("overflow-y-hidden", "touch-none", "overscroll-none")
+        }
+    }, [dropdownOpen]);
+
     return (
         <nav className={`fixed top-0 left-0 z-40 w-full py-4 flex justify-center transition-all px-10 ${dropdownOpen ? "text-black" : ''}`}>
-            <h1 className={`z-40 text-2xl font-semibold absolute left-10 ${dropdownOpen ? "text-black" : ''}`}>Tand Trygg</h1>
+            <div onClick={closeDropdown} className={`OVERLAY absolute top-0 left-0 bg-black opacity-40 w-full h-screen ${!dropdownOpen ? "hidden" : ''}`}></div>
+           <h1 className={`z-40 text-2xl font-semibold absolute left-10 ${dropdownOpen ? "text-black" : ''}`}>Tand Trygg</h1>
             <div className="flex gap-10">
                 <div onClick={handleClick} className={`flex items-center z-40 cursor-pointer transition-all ${dropdownOpen ? "text-black" : ''} relative hover:cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-200 before:absolute before:bg-teal-400 before:origin-center before:h-[1px] before:w-0 hover:before:w-[55%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-200 after:absolute after:bg-teal-400 after:origin-center after:h-[1px] after:w-0 hover:after:w-[55%] after:bottom-0 after:right-[50%]`}>
                     <p>Behandlingar</p>
@@ -31,7 +44,7 @@ const Navbar = () => {
                         {dropdownOpen && <Image className="w-full h-auto pb-1" src={arrowBlack} alt="arrow" />}
                     </div>
                     </div>
-                <div className={`absolute left-0 flex w-full justify-center bg-neutral-100 text-black transition-transform duration-300 pt-[100px] pb-6 ${!dropdownOpen ? "-translate-y-[200%]" : "-translate-y-10"}`}>
+                <div className={`absolute z-60 left-0 flex w-full justify-center bg-neutral-100 text-black transition-transform duration-300 pt-[100px] pb-6 ${!dropdownOpen ? "-translate-y-[200%]" : "-translate-y-10"}`}>
                     <div className="flex flex-col">
 
                         <Link className="px-20 flex hover:bg-teal-100 rounded-xl py-2 transition-all" href={'/emergency'}>
