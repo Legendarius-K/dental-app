7315b5b02b1279f2fd7f963b2e8e19907f7526dd
Darius K <darius.kaya@gmail.com>
2024-09-18 15:05:56 +0200

    hamburger menu left

--- src/components/BookButton/index.tsx
+++ src/components/BookButton/index.tsx
@@ -11,7 +11,7 @@ const BookButton = ({ text, buttonStyle }: BookButtonProps) => {
         //     {text}
         // </button>
 
-        <Link href="/" className={`z-20 relative inline-flex items-center justify-start inline-block overflow-hidden transition-all bg-teal-300 rounded-full hover:bg-teal-500 group px-5 py-3 ${buttonStyle}`}>
+        <Link href="/" className={`font-medium z-20 relative inline-flex items-center justify-start inline-block overflow-hidden transition-all bg-teal-300 rounded-full hover:bg-teal-500 group ${buttonStyle}`}>
             <span className="absolute inset-0 border-0 group-hover:border-[25px] ease-linear duration-100 transition-all border-teal-500 rounded-full"></span>
             <span className="relative w-full text-center text-neutral-800 transition-colors duration-200 ease-in-out group-hover:text-white">{text}</span>
         </Link>
--- src/components/ContactButton/index.tsx
+++ src/components/ContactButton/index.tsx
@@ -11,7 +11,7 @@ const ContactButton = ({ text, buttonStyle }: ContactButtonProps) => {
         //     {text}
         // </button>
 
-        <Link href="/" className={` z-20 relative inline-flex items-center justify-start inline-block overflow-hidden transition-all bg-neutral-100 rounded-full hover:bg-neutral-400 group px-5 py-3 ${buttonStyle}`}>
+        <Link href="/" className={`font-medium z-20 relative inline-flex items-center justify-start inline-block overflow-hidden transition-all bg-neutral-100 rounded-full hover:bg-neutral-400 group ${buttonStyle}`}>
             <span className="absolute inset-0 border-0 group-hover:border-[25px] ease-linear duration-100 transition-all border-neutral-400 rounded-full"></span>
             <span className="relative w-full text-center text-neutral-800 transition-colors duration-200 ease-in-out group-hover:text-white">{text}</span>
         </Link>
