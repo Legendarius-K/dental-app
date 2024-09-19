6367c51f903dfa840b82e729293ac791b04cffe1
Davide Baldi <d.baldi88@gmail.com>
2024-09-19 14:32:06 +0200

    saved new picture

--- src/components/HeroPrices/index.tsx
+++ src/components/HeroPrices/index.tsx
@@ -6,9 +6,9 @@ const HeroPrices = () => {
     return (
         <section
             className="relative w-full h-[600px] bg-cover bg-center text-neutral-100 font-sans"
-            style={{ backgroundImage: "url('/images/hero-price.jpg')" }}
+            style={{ backgroundImage: "url('/images/hero-price2.jpg')" }}
         >
-            <div className="absolute inset-0 bg-black opacity-30 z-10"></div>
+            <div className="absolute inset-0 bg-black opacity-60 z-10"></div>
             <div className=" ml-[130px]">
                 <h1 className="relative z-20 text-7xl font-semibold max-w-[500px] pt-[190px]">Våra priser </h1>
                 <h2 className="relative z-20 text-xl max-w-[500px] pt-6 font-light">Vårt mål på Aqua Dental är att erbjuda den bästa tandvården till rätt pris. Detta</h2>
