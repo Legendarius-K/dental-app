75813dd2658f3b20ff99af42e3e8d08971e280a9
Darius K <darius.kaya@gmail.com>
2024-09-22 19:32:37 +0200

    hero button animation

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import BookButton from "../BookButton";
 import ContactButton from "../ContactButton";
 import Navbar from "../Navbar";
+import { motion } from "framer-motion";
 
 const Hero = () => {
     return (
@@ -14,10 +17,22 @@ const Hero = () => {
                 <h1 className="relative z-20 text-5xl md:text-7xl max-w-[500px] pt-52 md:pt-[190px] font-nunito font-bold">Tranås <span className="relative before:content-[''] before:absolute before:w-full before:h-[2px] before:bg-teal-300 md:before:bottom-5 before:bottom-4 before:left-0 before:translate-y-1">bästa</span> tandvård</h1>
                 <h2 className="relative z-20 md:text-lg max-w-[500px] pt-6 font-light">Ny fräsch klinik mitt i Tranås. Boka idag så bjuder vi på de 100 första besöken!</h2>
             </div>
-            <div className="flex f md:flex-row gap-6 mt-10 md:ml-[80px] xl:ml-[130px] mx-6">
+            <motion.div className="relative z-20 flex f md:flex-row gap-6 mt-10 md:ml-[80px] xl:ml-[130px] mx-6"
+                initial={{
+                    opacity: 0,
+                    y: 50
+                }}
+                whileInView={{
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                        duration: 0.5
+                    }
+                }}
+                viewport={{ once: true }}>
                 <BookButton text="BOKA ONLINE" buttonStyle="text-sm md:text-xl py-1 md:py-3 w-[200px] h-12 md:h-auto px-3" />
                 <ContactButton text="KONTAKTA OSS" buttonStyle="text-sm md:text-xl py-1 md:py-3 w-[200px] h-12 md:h-auto px-3" />
-            </div>
+            </motion.div>
             <div className="flex z-30 absolute inset-x-0 bottom-0 h-32 md:h-16 flex-wrap md:flex-nowrap justify-around items-center text-pt-navyblue p-4 bg-pt-gray2 bg-neutral-100 text-neutral-800">
                 <div className="w-[139px] md:w-auto  md:text-left mb-2 md:mb-0 text-sm md:text-[16px]">
                     <p>&#8226; Enkel bokning</p>
