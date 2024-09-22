fcdcfbbdb0a354df95b4ece2ac4ab9806303c665
Darius K <darius.kaya@gmail.com>
2024-09-22 19:33:29 +0200

    button click animation

--- src/components/BookButton/index.tsx
+++ src/components/BookButton/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 type BookButtonProps = {
     text: string
@@ -7,14 +8,12 @@ type BookButtonProps = {
 
 const BookButton = ({ text, buttonStyle }: BookButtonProps) => {
     return (
-        // <button className={`font-sans  relative z-20 bg-teal-300 text-neutral-800 rounded-3xl shadow-xl ${buttonStyle}`}>
-        //     {text}
-        // </button>
-
-        <Link href="/" className={`font-medium z-20 relative inline-flex items-center justify-start inline-block overflow-hidden transition-all bg-teal-300 rounded-full hover:bg-teal-500 group ${buttonStyle}`}>
-            <span className="absolute inset-0 border-0 group-hover:border-[25px] ease-linear duration-100 transition-all border-teal-500 rounded-full"></span>
-            <span className="relative w-full text-center text-neutral-800 transition-colors duration-200 ease-in-out group-hover:text-white">{text}</span>
-        </Link>
+        <motion.div whileTap={{ scale: 0.85 }}>
+            <Link href="/" className={`font-medium z-20 relative inline-flex items-center justify-start inline-block overflow-hidden transition-all bg-teal-300 rounded-full hover:bg-teal-500 group ${buttonStyle}`}>
+                <span className="absolute inset-0 border-0 group-hover:border-[25px] ease-linear duration-100 transition-all border-teal-500 rounded-full"></span>
+                <span className="relative w-full text-center text-neutral-800 transition-colors duration-200 ease-in-out group-hover:text-white">{text}</span>
+            </Link>
+        </motion.div>
     )
 };
 
