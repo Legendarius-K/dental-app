99ea93c97a865973d351cfd2ce5e229a431e93c9
Darius K <darius.kaya@gmail.com>
2024-09-22 19:40:42 +0200

    -

--- src/components/BookButton/index.tsx
+++ src/components/BookButton/index.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { motion } from "framer-motion";
 
 type BookButtonProps = {
     text: string
@@ -8,12 +7,10 @@ type BookButtonProps = {
 
 const BookButton = ({ text, buttonStyle }: BookButtonProps) => {
     return (
-        <motion.div whileTap={{ scale: 0.85 }}>
-            <Link href="/" className={`font-medium z-20 relative inline-flex items-center justify-start inline-block overflow-hidden transition-all bg-teal-300 rounded-full hover:bg-teal-500 group ${buttonStyle}`}>
-                <span className="absolute inset-0 border-0 group-hover:border-[25px] ease-linear duration-100 transition-all border-teal-500 rounded-full"></span>
-                <span className="relative w-full text-center text-neutral-800 transition-colors duration-200 ease-in-out group-hover:text-white">{text}</span>
-            </Link>
-        </motion.div>
+        <Link href="/" className={`font-medium z-20 relative inline-flex items-center justify-start inline-block overflow-hidden transition-all bg-teal-300 rounded-full hover:bg-teal-500 group ${buttonStyle}`}>
+            <span className="absolute inset-0 border-0 group-hover:border-[25px] ease-linear duration-100 transition-all border-teal-500 rounded-full"></span>
+            <span className="relative w-full text-center text-neutral-800 transition-colors duration-200 ease-in-out group-hover:text-white">{text}</span>
+        </Link>
     )
 };
 
--- src/components/ContactButton/index.tsx
+++ src/components/ContactButton/index.tsx
@@ -7,10 +7,6 @@ type ContactButtonProps = {
 
 const ContactButton = ({ text, buttonStyle }: ContactButtonProps) => {
     return (
-        // <button className={`font-sans  relative z-20 bg-neutral-100 text-neutral-800 rounded-3xl shadow-xl ${buttonStyle}`}>
-        //     {text}
-        // </button>
-
         <Link href="/" className={`font-medium z-20 relative inline-flex items-center justify-start inline-block overflow-hidden transition-all bg-neutral-100 rounded-full hover:bg-neutral-400 group ${buttonStyle}`}>
             <span className="absolute inset-0 border-0 group-hover:border-[25px] ease-linear duration-100 transition-all border-neutral-400 rounded-full"></span>
             <span className="relative w-full text-center text-neutral-800 transition-colors duration-200 ease-in-out group-hover:text-white">{text}</span>
