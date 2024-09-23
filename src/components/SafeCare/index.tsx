0137e2c9e2efc86d026a78e913fc89c0e6faa913
Darius K <darius.kaya@gmail.com>
2024-09-23 10:33:06 +0200

    -

---
+++ src/components/SafeCare/index.tsx
@@ -0,0 +1,12 @@
+
+
+const SafeCare = () => {
+    return (
+        <section className="bg-orange-400">
+            <h2>Trygg och vänlig tandvård</h2>
+            <p>Det är vårt outtröttliga mål. Vi vill göra det lättare, tryggare och trevligare att gå till tandläkaren.</p>
+        </section>
+    )
+};
+
+export default SafeCare
