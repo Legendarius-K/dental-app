ab55f04148082b5fa01ad0a63988af8b8312cf3a
davidebaldi <d.baldi88@gmail.com>
2024-09-25 09:17:29 +0200

    good enough let s merge

--- src/components/Payments/index.tsx
+++ src/components/Payments/index.tsx
@@ -5,16 +5,19 @@
 const Payments = () => {
     return (
         <section className="flex flex-col md:flex-row bg-accent max-h-[600px]">
-            <div className="basis-2/3">
-            <img src="../../images/payment.jpg" alt="" />
+            <div className="basis-1/2 max-h-[400px]  md:max-h-full ">
+            <img src="../../images/payment.jpg" alt="" className=" max-h-[400px] w-full md:max-h-full "/>
             </div>
             
-            <div className="basis-1/2">
-                <h2>Alltid hos Tand Trygg</h2>
-                <p>- Tydliga och transparenta priser</p>
-                <p>- Inga dolda avgifter</p>
-                <p>- Samma pris sju dagar i veckan</p>
-                <p>- Specialisttandvård med hög tillgänglighet</p>
+            <div className="basis-1/2 p-8 flex flex-col items-center justify-center">
+                <h2 className="text-bluegray text-2xl md:text-4xl lg:text-5xl md:mb-8 font-semibold text-center max-w-[800px]">Alltid hos Tand Trygg</h2>
+                <div>
+                    <p className="text-white font-sans lg:pt-4 sm:text-lg lg:text-2xl">- Tydliga och transparenta priser</p>
+                    <p className="text-white font-sans lg:pt-4 sm:text-lg lg:text-2xl">- Inga dolda avgifter</p>
+                    <p className="text-white font-sans lg:pt-4 sm:text-lg lg:text-2xl">- Samma pris sju dagar i veckan</p>
+                    <p className="text-white font-sans lg:pt-4 sm:text-lg lg:text-2xl">- Specialisttandvård med hög tillgänglighet</p>
+                </div>
+                
 
             </div>
         </section>
