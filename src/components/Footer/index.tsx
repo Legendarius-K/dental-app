fb3c045442d8e28eff3a6f1dd1701d562cb4d6ed
davidebaldi <d.baldi88@gmail.com>
2024-09-25 08:08:29 +0200

    fixer links and logo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <footer className={`w-full py-16 mx-auto px-8 text-textwhite bg-bluegray border-t-2 font-sans`}>
       <div className="container mx-auto px-8 md:px-0 lg:px-8 flex flex-col md:flex-row justify-between items-start">
         <div className="flex flex-col md:flex-row items-start ">
-          <img src="../../Images/logo.png" alt="Logo" className="w-[80px] h-[80px] mb-4 md:mb-0 md:mr-4 rounded-full" />
+          <img src="../../images/logo.png" alt="Logo" className="w-[80px] h-[80px] mb-4 md:mb-0 md:mr-4 rounded-full" />
           <div className="text-sm mt-4 md:mt-0 md:ml-8 align-top">
             <p className="p1">Address:</p>
             <p>Sankt Eriksgatan 500 | 113 43</p>
@@ -22,10 +22,10 @@ const Footer = () => {
             <p>info@tandtrygg.se</p>
             <div className="flex space-x-2 mt-2">
               <Link href="https://instagram.com" target="_blank" aria-label="Instagram">
-                <img src="../../Images/instagram-icon.png" alt="Instagram" className="w-6 h-6" />
+                <img src="../../images/instagram-icon.png" alt="Instagram" className="w-6 h-6" />
               </Link>
               <Link href="https://linkedin.com" target="_blank" aria-label="LinkedIn">
-                <img src="../../Images/linkedIn-icon.png" alt="LinkedIn" className="w-6 h-6" />
+                <img src="../../images/linkedIn-icon.png" alt="LinkedIn" className="w-6 h-6" />
               </Link>
             </div>
           </div>
@@ -35,8 +35,8 @@ const Footer = () => {
           <ul className="flex flex-col md:flex-row align-baseline space-y-4 md:space-y-0 md:py-4">
             <li><Link href={``} className="md:pr-4 p2 font-size-[14px] md:underline">Boka tid</Link></li>
             <li><Link href={``} className="md:pr-4 p2 font-size-[14px] md:underline">Kontakta oss</Link></li>
-            <li><Link href={``} className="md:pr-4 p2 font-size-[14px] md:underline">Om oss</Link></li>
-            <li><Link href={``} className="md:pr-4 p2 font-size-[14px] md:underline">Priser</Link></li>
+            <li><Link href={`/about_us`} className="md:pr-4 p2 font-size-[14px] md:underline">Om oss</Link></li>
+            <li><Link href={`/prices`} className="md:pr-4 p2 font-size-[14px] md:underline">Priser</Link></li>
           </ul>
         </div>
       </div>
