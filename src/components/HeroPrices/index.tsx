7550e1da459f0c495afff88874a50ff03b56e92d
davidebaldi <d.baldi88@gmail.com>
2024-09-25 11:21:35 +0200

    complete responsvness and hero at price sections

--- src/components/HeroPrices/index.tsx
+++ src/components/HeroPrices/index.tsx
@@ -1,29 +1,31 @@
+
+
 import BookButton from "../BookButton";
 import ContactButton from "../ContactButton";
 import Navbar from "../Navbar";
 
 
-const HeroPrices = () => {
+const Hero = () => {
     return (
         <section
-            className="relative w-full h-[600px] bg-cover bg-center text-textwhite font-sans"
+            className="relative w-full h-[550px] max-h-screen bg-cover bg-center text-neutral-100 font-sans"
             style={{ backgroundImage: "url('/images/hero-price2.jpg')" }}
         >
-            <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
-            <div className=" ml-[130px]">
-                <h1 className="relative z-20 text-7xl font-semibold max-w-[500px] pt-[190px]">Våra priser </h1>
-                <h2 className="relative z-20 text-xl max-w-[500px] pt-6 font-light">Vårt mål på Aqua Dental är att erbjuda den bästa tandvården till rätt pris. Detta</h2>
-                <h2 className="relative z-20 text-xl max-w-[500px] pt-6 font-light">gör vi genom att tillhandahålla bra priser men framförallt genom vår extremt höga kvalitet. </h2>
-                <div className="flex gap-6 mt-10">
-                    <BookButton text="BOKA ONLINE" buttonStyle="text-xl py-3 w-[200px] font-bold" />
-                    <ContactButton text="KONTAKTA OSS" buttonStyle="text-xl py-3 w-[200px]" />
-                </div>
-            </div>
             <Navbar />
+            <div className="absolute inset-0 bg-black opacity-40 z-10"></div>
+            <div className="ml-6 mr-6 md:ml-[80px] xl:ml-[130px]">
+                <h1 className="relative z-20 text-5xl md:text-7xl max-w-[500px] pt-52 md:pt-[190px] font-nunito font-bold">Våra priser</h1>
+                <h2 className="relative z-20 md:text-lg max-w-[500px] pt-6 font-light">Vårt mål på Trygg Tand är att erbjuda den bästa tandvården till rätt pris. Detta gör vi genom att tillhandahålla bra priser men framförallt genom vår extremt höga kvalitet</h2>
+            </div>
+            <div className="relative z-20 flex f md:flex-row gap-6 mt-10 md:ml-[80px] xl:ml-[130px] mx-6">
+                <BookButton text="BOKA ONLINE" buttonStyle="text-sm md:text-xl py-1 md:py-3 w-[200px] h-12 md:h-auto px-3 font-bold" />
+                <ContactButton text="KONTAKTA OSS" buttonStyle="text-sm md:text-xl py-1 md:py-3 w-[200px] h-12 md:h-auto px-3" />
+            </div>
             
-
         </section>
     );
 };
 
-export default HeroPrices;
+export default Hero;
+
+
--- src/components/PriceMap/index.tsx
+++ src/components/PriceMap/index.tsx
@@ -7,7 +7,7 @@ const PriceMap = () => {
     return (
         <section className="flex flex-col md:flex-row bg-white max-h-[1000px]">
             <div className="basis-1/2 p-8 flex flex-col items-center justify-center">
-                <h2 className="text-black text-4xl lg:text-4xl md:mb-8 text-center max-w-[800px]">Det får du för pengarna</h2>
+                <h2 className="text-black text-4xl lg:text-4xl mb-4 md:mb-8 text-center max-w-[800px]">Det får du för pengarna</h2>
                 <div>
                     <p className="text-black font-sans lg:pt-4 sm:text-md  lg:max-w-[550px] text-justify">Vad prisvärd <a href="" className="font-bold text-accent hover:text-darkblue">tandvård</a> egentligen innebär beror ju så klart på vem du frågar. För oss handlar det om kvalitativa behandlingar och ingrepp som håller över tid. Som minimerar risken för komplikationer, återbesök och upprepande åtgärder</p>
                     <p className="text-black font-sans lg:pt-4 sm:text-md  lg:max-w-[550px] text-justify">Att verka förebyggande och långsiktigt är därför några av våra viktigaste åtaganden. Vårt löfte är att aldrig kompromissa med <a href="" className="font-bold text-accent hover:text-darkblue">trygghet, kvalitet och patientsäkerhet.</a> </p>
@@ -15,7 +15,7 @@ const PriceMap = () => {
                 </div>
             </div>
             <div className="basis-1/2 p-8 flex flex-col ">
-                <h2 className="text-black text-4xl lg:text-4xl md:mb-8 text-center max-w-[800px]">Hitta en ledig tid</h2>
+                <h2 className="text-black text-4xl lg:text-4xl mb-4 md:mb-8 text-center max-w-[800px]">Hitta en ledig tid</h2>
                 <div className='bg-bluegray flex justify-between items-center'>
                     <h2 className=" text-white text-2xl md:text-4xl lg:text-5xl md:mb-8 text-center max-w-[800px] font-sans my-4 pl-2">Välj en klinik </h2>
                     <img src={Phone.src} alt="" className='w-[40px] h-[40px] bg-mainblue hover:bg-white rounded-full border-2 p-1 m-6 cursor-pointer' />
