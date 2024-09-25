90e584ec9cf77b91bcb5fdd11e8ada4a2895c6c2
davidebaldi <d.baldi88@gmail.com>
2024-09-25 18:09:45 +0200

    save 10 mock staff data

---
+++ src/components/StaffContainer/index.tsx
@@ -0,0 +1,16 @@
+
+
+
+const StaffContainer = () => {
+    return (
+        <section className="bg-bluegray h-[700px] flex flex-col items-center px-20">
+        <h2 className="text-white text-4xl lg:text-6xl m-6 text-center max-w-[800px]">Vi som jobbar här</h2>
+        <div className="flex flex-col md:flex-row w-full">
+            <h2 className="text-white text-4xl lg:text-4xl m-6 font-semibold basis-1/2">Det är vi som är Tand Trygg </h2>
+            <p className="text-white text-xl basis-1/2 ">Vi är ett team av tandläkare, tandhygienister, tandsköterskor och tandtekniker med rätt erfarenhet och hög kompetens. Kontinuerlig kompetensutveckling är en naturlig del av vår vardag: i våra respektive professioner samt i användande av den toppmoderna tekniska utrustning vi har – och införskaffar vid behov. Tillsammans är det vi som är Aqua Dental. </p>
+        </div>
+        </section>
+    )
+}
+
+export default StaffContainer;
---
+++ utils/data.tsx
@@ -0,0 +1,53 @@
+const dentists = [
+    {
+      name: "Johan Andersson",
+      title: "Allmäntandläkare",
+      pictureUrl: "https://example.com/johan-andersson.jpg"
+    },
+    {
+      name: "Sofia Nilsson",
+      title: "Allmäntandläkare",
+      pictureUrl: "https://example.com/sofia-nilsson.jpg"
+    },
+    {
+      name: "Erik Johansson",
+      title: "Allmäntandläkare",
+      pictureUrl: "https://example.com/erik-johansson.jpg"
+    },
+    {
+      name: "Emma Karlsson",
+      title: "Tandhygienist",
+      pictureUrl: "https://example.com/emma-karlsson.jpg"
+    },
+    {
+      name: "Mats Svensson",
+      title: "Tandhygienist",
+      pictureUrl: "https://example.com/mats-svensson.jpg"
+    },
+    {
+      name: "Anna Larsson",
+      title: "Tandhygienist",
+      pictureUrl: "https://example.com/anna-larsson.jpg"
+    },
+    {
+      name: "Fredrik Lindberg",
+      title: "Specialisttandläkare i käkkirurgi",
+      pictureUrl: "https://example.com/fredrik-lindberg.jpg"
+    },
+    {
+      name: "Maria Olsson",
+      title: "Tantsköterska",
+      pictureUrl: "https://example.com/maria-olsson.jpg"
+    },
+    {
+      name: "Peter Berg",
+      title: "Tantsköterska",
+      pictureUrl: "https://example.com/peter-berg.jpg"
+    },
+    {
+      name: "Linda Holm",
+      title: "Tantsköterskatist",
+      pictureUrl: "https://example.com/linda-holm.jpg"
+    }
+  ];
+  
