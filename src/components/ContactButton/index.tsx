5b29a03436ad5a96f6025261082bebd924b81168
Darius K <darius.kaya@gmail.com>
2024-09-26 15:16:05 +0200

    done i think

--- src/components/ContactButton/index.tsx
+++ src/components/ContactButton/index.tsx
@@ -7,8 +7,8 @@ type ContactButtonProps = {
 
 const ContactButton = ({ text, buttonStyle }: ContactButtonProps) => {
     return (
-        <Link href="/" className={`z-20 relative inline-flex items-center justify-start inline-block overflow-hidden transition-all bg-neutral-100 rounded-full hover:bg-darkblue group ${buttonStyle}`}>
-            <span className="absolute inset-0 border-0 group-hover:border-[25px] ease-linear duration-100 transition-all border-darkblue rounded-full"></span>
+        <Link href="/" className={`z-20 relative inline-flex items-center justify-start inline-block overflow-hidden transition-all duration-200 bg-neutral-100 rounded-full hover:bg-darkblue group ${buttonStyle}`}>
+            <span className="absolute inset-0 border-0 group-hover:border-[25px] ease-linear duration-200 transition-all border-darkblue rounded-full"></span>
             <span className="relative w-full text-center text-neutral-800 transition-colors duration-200 ease-in-out group-hover:text-white">{text}</span>
         </Link>
     )
