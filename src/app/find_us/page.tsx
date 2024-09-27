366954b1c8cf23ee31b8bf1cd981597a36b02d7a
Darius K <darius.kaya@gmail.com>
2024-09-27 07:56:48 +0200

    m

--- src/app/find_us/page.tsx
+++ src/app/find_us/page.tsx
@@ -1,10 +1,20 @@
+import HeroFindUs from "@/components/HeroFindUs";
 
 
 const find_us = () => {
     return (
-        <>
-         Hitta oss   
-        </>
+        <section>
+            <HeroFindUs/>
+            <div className="flex justify-around p-10">
+                <div className="bg-lightblue">
+                    <h3>Tand Trygg</h3>
+                    <p></p>
+                </div>
+                <div className="bg-lightblue">
+                    Telefontider
+                </div>
+            </div>
+        </section>
     )
 };
 
