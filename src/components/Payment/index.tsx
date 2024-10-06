0660e6ed2d26d340d4135050fa55880a8d1697d7
Davide Baldi <d.baldi88@gmail.com>
2024-10-06 08:25:28 +0200

    save

--- src/components/Payment/index.tsx
+++ src/components/Payment/index.tsx
@@ -4,7 +4,7 @@ const Payment = () => {
     return (
         <section className="flex flex-col md:flex-row">
             <img src="../images/payment.jpg" alt="" />
-            <div className="flex flex-col">
+            <div className="flex flex-col bg-[#AF5E5A]">
                 <h2>Alltid hos Tand Trygg</h2>
             </div>
         </section>
