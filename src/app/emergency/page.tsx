d72e06c95b71d0dd3e25a824c490e3ff44a270b6
Darius K <darius.kaya@gmail.com>
2024-10-07 21:23:40 +0200

    -

--- src/app/emergency/page.tsx
+++ src/app/emergency/page.tsx
@@ -12,7 +12,7 @@ const emergency = () => {
         <Hero/>
             <main className="bg-white flex flex-col items-center">
                 <div className="md:pt-16 flex flex-col items-center max-w-[1150px] p-6 md:p-10">
-                    <h2 className="mt-20 md:my-6 text-4xl md:text-5xl font-semibold text-bluegray">Akut tandvård</h2>
+                    {/* <h2 className="mt-20 md:my-6 text-4xl md:text-5xl font-semibold text-bluegray">Akut tandvård</h2> */}
                     <section className="w-full flex flex-col-reverse md:flex-row gap-16 my-10">
                         <div className="md:w-1/2 w-full flex flex-col gap-5 font-sans">
                             <h3 className="text-2xl md:text-3xl font-nunito font-semibold text-center md:text-left text-bluegray">Plötslig tandvärk?</h3>
@@ -21,7 +21,7 @@ const emergency = () => {
                             <p>Hos oss möts du av erfarna tandläkare och tandsköterskor som använder den senaste tekniken. Även om du har tandvärk eller lider av tandläkarskräck, är vi experter på att skapa en behaglig och trygg upplevelse under hela ditt besök.</p>
                             <p>Många akuttandläkare har extra avgifter på kvällar och helger, men på Aqua Dental har vi tagit bort dessa tillägg. Hos oss betalar du samma pris för tandvård oavsett tid på dygnet eller veckodag.</p>
                             <div className="w-full flex justify-center md:justify-start">
-                                <BookButton text="Boka nu" buttonStyle="w-[300px] md:w-[200px] py-3 mt-10" />
+                                <BookButton text="Boka nu" buttonStyle="w-[300px] md:w-[200px] py-3 mt-10 shadow-lg" />
                             </div>
                         </div>
                         <div className="md:w-1/2 w-full">
