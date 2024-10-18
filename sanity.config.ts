ed045c5cc1cb67f672684e473f80676ccd476102
davidebaldi <d.baldi88@gmail.com>
2024-10-18 15:52:39 +0200

    sanity studio configurated

--- sanity.config.ts
+++ sanity.config.ts
@@ -12,17 +12,18 @@ import {structureTool} from 'sanity/structure'
 import {apiVersion, dataset, projectId} from './src/sanity/env'
 import {schema} from './src/sanity/schemaTypes'
 import {structure} from './src/sanity/structure'
+import schemas from '@/sanity/schemas'
 
 export default defineConfig({
   basePath: '/studio',
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
-  schema,
   plugins: [
     structureTool({structure}),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
   ],
+  schema: {types: schemas}
 })
---
+++ src/sanity/schemas/project-schema.ts
@@ -0,0 +1,45 @@
+import treatments from "@/app/treatments/page";
+import { source } from "framer-motion/client";
+import { title } from "process";
+
+const prices = {
+    name: "prices",
+    title: "Prices",
+    type: "document",
+    fields: [
+        {
+            name: "treatment",
+            title: "Treatment",
+            type: "string"
+        },
+        {
+            name: "slug",
+            title: "Slug",
+            type: "slug", //inbuilt typ for sanity
+            option: {source: "treatment"} 
+        },
+        {
+            name: "code",
+            title: "Code- Åtgärdsnr",
+            type: "number"
+        },
+        {
+            name: "amount_under_3000",
+            title: "amount_under_3000",
+            type: "number"
+        },
+        {
+            name: "amount_3000_15000",
+            title: "amount_3000_15000",
+            type: "number"
+        },
+        {
+            name: "amount_over_15000",
+            title: "amount_over_15000",
+            type: "number"
+        }
+
+    ]
+}
+
+export default prices;
