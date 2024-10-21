49614be5c84f8edb32e31ea60622b4b170551fb4
davidebaldi <d.baldi88@gmail.com>
2024-10-21 10:53:25 +0200

    styling good enough

--- src/components/PriceList/index.tsx
+++ src/components/PriceList/index.tsx
@@ -6,8 +6,18 @@ const PriceList= async () => {
     const prices = await getPrices();
 
     return (
-        <div>
-            {prices.map((price, index)=> <h4 key={index}>{price.title}</h4>)}
+        <div className="text-[16px] flex flex-col font-bold p-8">
+            {prices.map((price, index)=> 
+            <div className="grid grid-cols-5 pt-5">
+                <p key={index}>{price.code}</p>
+                <p className="col-span-3" key={index}>{price.treatment}</p>
+                <div className="flex flex-col sm:grid sm:grid-cols-3 ">
+                    <p key={index} className="text-right">{price.amount_under_3000}kr</p>
+                    <p key={index} className="text-bluegray text-right">{price.amount_3000_15000}kr</p>
+                    <p key={index} className="text-accent text-right">{price.amount_over_15000}kr</p>
+                </div>
+            </div>
+        )}
         </div>
     )
 }
