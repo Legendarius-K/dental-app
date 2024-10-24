846ed1172f55dc56dc54d92d54e9c5a14c9747ba
davidebaldi <d.baldi88@gmail.com>
2024-10-24 08:57:58 +0200

    fixed unique index to the sanity div

--- src/components/PriceList/index.tsx
+++ src/components/PriceList/index.tsx
@@ -8,13 +8,13 @@ const PriceList= async () => {
     return (
         <div className="text-[16px] flex flex-col font-bold p-8">
             {prices.map((price, index)=> 
-            <div className="grid grid-cols-4 pt-5">
-                <p key={index} className="pl-8">{price.code}</p>
+            <div key={index} className="grid grid-cols-4 pt-5">
+                <p className="pl-8">{price.code}</p>
                 <p className="col-span-2" key={index}>{price.treatment}</p>
                 <div className="flex flex-col sm:grid sm:grid-cols-3 ">
-                    <p key={index} className="text-right">{price.amount_under_3000}kr</p>
-                    <p key={index} className="text-bluegray text-right">{price.amount_3000_15000}kr</p>
-                    <p key={index} className="text-accent text-right">{price.amount_over_15000}kr</p>
+                    <p className="text-right">{price.amount_under_3000}kr</p>
+                    <p className="text-bluegray text-right">{price.amount_3000_15000}kr</p>
+                    <p className="text-accent text-right">{price.amount_over_15000}kr</p>
                 </div>
             </div>
         )}
