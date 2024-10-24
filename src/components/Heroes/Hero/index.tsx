002b1703e23f6ddb1817ce1eaab2586528a83e54
davidebaldi <d.baldi88@gmail.com>
2024-10-24 11:11:20 +0200

    fixed font on mobile

--- src/components/Heroes/Hero/index.tsx
+++ src/components/Heroes/Hero/index.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         >
             <div className="absolute inset-0 bg-black opacity-40 z-10"></div>
             <div className="ml-6 mr-6 md:ml-[80px] xl:ml-[130px]">
-                <h1 className="relative z-20 text-4xl sm:text-5xl md:text-7xl max-w-[160px] sm:max-w-[450px] pt-[230px] md:pt-[150px] font-nunito font-bold">Tandvård för ett <span className="underline decoration-[#84DAE0] decoration-2 underline-offset-4">friskt</span> och <span className="underline decoration-[#84DAE0] decoration-2 underline-offset-4">hållbart</span>  </h1>
+                <h1 className="relative z-20 text-3xl sm:text-5xl md:text-7xl max-w-[160px] sm:max-w-[450px] pt-[230px] md:pt-[150px] font-nunito font-bold">Tandvård för ett <span className="underline decoration-[#84DAE0] decoration-2 underline-offset-4">friskt</span> och <span className="underline decoration-[#84DAE0] decoration-2 underline-offset-4">hållbart</span>  </h1>
                 <h2 className="relative z-20 md:text-lg max-w-[500px] pt-6 font-light"><span className="font-bold	">Gratis första konsultation</span>– För alla nya patienter!</h2>
                 <h2 className="relative z-20 md:text-lg max-w-[500px] pt-6 font-light"><span className="font-bold	">Familjerabatt</span> – Få rabatt när hela familjen bokar sina besök samtidigt.</h2>
             </div>
