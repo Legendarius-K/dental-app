0ddbdfd59710ee97b40b00f7680b6cc4108b2593
Darius K <darius.kaya@gmail.com>
2024-10-27 16:37:12 +0100

    darker secondary

--- src/components/GetInTouch/index.tsx
+++ src/components/GetInTouch/index.tsx
@@ -6,12 +6,12 @@ const GetInTouch = () => {
             <div className="w-full max-w-[1400px]">
                 <h2 className="text-center text-3xl md:text-4xl lg:text-5xl font-nunito font-semibold mt-14">Välkommen att höra av dig!</h2>
                 <div className="flex flex-col sm:flex-row p-4 pb-24  sm:p-10 sm:pb-28 mt-8">
-                    <div className="flex flex-col justify-between w-full p-0 h-[450px]">
+                    <div className="flex flex-col justify-between w-full p-0 h-[440px]">
                         <ContactSquare title="Tand Trygg" two={contact.adress} three={contact.zipCode} phone={contact.phone} email={contact.email} />
                         <ContactSquare title="Telefontider" two="Måndag-onsdag: 10-15" three="Torsdag: 10-16" four="Fredag: 10-14" five="Helg: stängt" />
                     </div>
-                    <div className="box-border w-[100%] mt-12 sm:mt-0 border-4 border-accent rounded">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31818.04789364442!2d14.93595308845188!3d58.03183079054311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465987fd56607ce5%3A0x1f93fa2a89c6fcd!2zVHJhbsOlcw!5e1!3m2!1ssv!2sse!4v1727419806771!5m2!1ssv!2sse" width="100%" height="442" style={{ border: 0, }}></iframe>
+                    <div className="box-border w-[100%] mt-12 sm:mt-0 rounded">
+                        <iframe className="rounded-lg" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31818.04789364442!2d14.93595308845188!3d58.03183079054311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465987fd56607ce5%3A0x1f93fa2a89c6fcd!2zVHJhbsOlcw!5e1!3m2!1ssv!2sse!4v1727419806771!5m2!1ssv!2sse" width="100%" height="442" style={{ border: 0, }}></iframe>
                     </div>
                 </div>
             </div>
