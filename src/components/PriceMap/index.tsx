a911bb1dedc45c8b9706e9c4bead050e3c49779e
darius <darius.kaya@gmail.com>
2024-11-25 11:57:07 +0100

    fixed prices page and added miro text

--- src/components/PriceMap/index.tsx
+++ src/components/PriceMap/index.tsx
@@ -1,38 +1,102 @@
-import Phone from '../../../public/images/telephone.png';
-import BookButton from '../BookButton';
-
-
+import { PhoneCall } from "lucide-react";
+import Phone from "../../../public/images/telephone.png";
+import BookButton from "../BookButton";
 
 const PriceMap = () => {
-    return (
-        <section className="flex flex-col md:flex-row bg-white max-h-[1000px]">
-            <div className="basis-1/2 p-8 flex flex-col items-center justify-center">
-                <h2 className="text-black text-4xl lg:text-4xl mb-4 md:mb-8 text-center max-w-[800px]">Det får du för pengarna</h2>
-                <div>
-                    <p className="text-black font-sans lg:pt-4 sm:text-md  lg:max-w-[550px] text-justify">Vad prisvärd <a href="" className="font-bold text-accent hover:text-darkblue">tandvård</a> egentligen innebär beror ju så klart på vem du frågar. För oss handlar det om kvalitativa behandlingar och ingrepp som håller över tid. Som minimerar risken för komplikationer, återbesök och upprepande åtgärder</p>
-                    <p className="text-black font-sans lg:pt-4 sm:text-md  lg:max-w-[550px] text-justify">Att verka förebyggande och långsiktigt är därför några av våra viktigaste åtaganden. Vårt löfte är att aldrig kompromissa med <a href="" className="font-bold text-accent hover:text-darkblue">trygghet, kvalitet och patientsäkerhet.</a> </p>
-                    <p className="text-black font-sans lg:pt-4 sm:text-md  lg:max-w-[550px] text-justify"> Våra tandläkare har i snitt 20 års erfarenhet och var fjärde har en <a href="" className="font-bold text-accent hover:text-darkblue">specialistutbildning.</a> Vi är också stolta över att finnas tillgängliga 365 dagar utan någon helg- och kvällstaxa. </p>
-                </div>
-            </div>
-            <div className="basis-1/2 p-8 flex flex-col ">
-                <h2 className="text-black text-4xl lg:text-4xl mb-4 md:mb-8 text-center max-w-[800px]">Hitta en ledig tid</h2>
-                <div className='bg-bluegray flex justify-between items-center'>
-                    <h2 className=" text-white text-2xl md:text-4xl lg:text-5xl md:mb-8 text-center max-w-[800px] font-sans my-4 pl-2">Välj en klinik </h2>
-                    <img src={Phone.src} alt="" className='w-[40px] h-[40px] bg-mainblue hover:bg-white rounded-full border-2 p-1 m-6 cursor-pointer' />
-                </div>
-                <div className='flex my-4 xl:w-[75%]'>
-                    <p className="w-[80%] text-3xl lg:text-5xl p-2">Nörrköping</p>
-                    <BookButton text="BOKA HÄR" buttonStyle="text-sm md:text-xl py-1 md:py-3 w-[200px] h-12 md:h-auto px-3 font-bold" />
-                </div>
-                <div className='flex xl:w-[75%]'>
-                    <p className="w-[80%] text-3xl lg:text-5xl p-2 ">Tranås</p>
-                    <BookButton text="BOKA HÄR" buttonStyle="text-sm md:text-xl py-1 md:py-3 w-[200px] h-12 md:h-auto px-3 font-bold" />
-                </div>
-            </div>
+  return (
+    <section className="flex flex-col md:flex-row bg-textwhite py-12 pb-20 md:pb-16 md:py-16 lg:py-24 lg:pb-16">
+      <div className="w-full md:w-1/2  flex flex-col items-center justify-center px-4 md:px-8 pb-10">
+        <h2 className="font-semibold text-2xl md:text-3xl mb-4 md:mb-8 text-center ">
+          Det här får du hos Trygg Tandvård
+        </h2>
+        <div>
+          <p className="text-black font-sans lg:pt-4 sm:text-md  lg:max-w-[550px] text-justify my-3">
+            <span className="font-bold text-accent hover:text-darkblue">
+              Prisvärd tandvård {" "}
+            </span>
+            innebär olika saker för olika personer, men hos oss betyder det
+            kvalitet och hållbarhet. Vi fokuserar på behandlingar som är
+            långsiktiga och förebyggande för att minska risken för
+            komplikationer och onödiga återbesök.
+          </p>
+          <p className="text-black font-sans lg:pt-4 sm:text-md  lg:max-w-[550px] text-justify my-3">
+            <span className="font-bold text-accent hover:text-darkblue">
+              Vårt löfte
+            </span>{" "}
+            till dig är att alltid sätta trygghet, kvalitet och patientsäkerhet
+            först. Våra erfarna tandläkare har gedigen kompetens och arbetar för
+            att ge dig den bästa vården. Vi är dessutom tillgängliga året runt
+            och tar inte ut några extra avgifter för kvällar och helger.
+          </p>
+          <p className="text-black font-sans lg:pt-4 sm:text-md  lg:max-w-[550px] text-justify my-3">
+            Vi tror på att tandvård ska vara
+            <span className="font-bold text-accent hover:text-darkblue">
+              {" "}
+              enkel
+            </span>{" "}
+            och
+            <span className="font-bold text-accent hover:text-darkblue">
+              {" "}
+              tillgänglig
+            </span>{" "}
+            för alla. Därför erbjuder vi flexibla bokningsalternativ och en
+            välkomnande miljö där du alltid kan känna dig trygg och
+            omhändertagen. Hos oss är ditt leende vår högsta prioritet.
+          </p>
+        </div>
+      </div>
 
+      <div className="w-full md:w-1/2  md:border-l">
+        <h2 className="text-3xl bg-bluegray pl-6 text-textwhite py-2 font-nunito">
+          Hitta en ledig tid
+        </h2>
+        <div className="w-4/5 font-sans flex flex-col gap-4 mt-8 pl-6">
+          <p>
+            <span className="font-bold">Välj den klinik</span> som passar dig
+            bäst från listan nedan för att hitta en ledig tid. Du kan enkelt
+            boka din behandling på en tid som passar dig, oavsett om det är en
+            vardag eller helg.
+          </p>
+          <p>
+            <span className="font-bold">Vi ser fram emot</span> att välkomna dig
+            och göra ditt besök hos oss så smidigt och bekvämt som möjligt!
+          </p>
+        </div>
+        <div className="flex flex-col mt-12 pl-6 gap-4">
+          <BookButton
+            text="TRANÅS"
+            buttonStyle="text-lg md:text-xl py-2 md:py-3 w-[200px] px-3 font-medium"
+          />
+          <BookButton
+            text="NORRKÖPING"
+            buttonStyle="text-lg md:text-xl py-2 md:py-3 w-[200px] px-3 font-medium"
+          />
+        </div>
+      </div>
+      {/* <div className="mt-10 pr-6 basis-1/2 flex flex-col border-l border-bluegray">
+          <div className="px-6 bg-bluegray flex justify-between items-center">
+            <h2 className=" text-white text-xl md:text-2xl lg:text-3xl text-center max-w-[800px] font-nunito my-4 pl-2">
+              Välj en klinik{" "}
+            </h2>
+            <PhoneCall size={40} className="text-textwhite" />
+          </div>
+          <div className="flex my-4 xl:w-[75%]">
+            <p className="w-[80%] text-xl lg:text-2xl p-2">Norrköping</p>
+            <BookButton
+              text="BOKA HÄR"
+              buttonStyle="text-sm md:text-xl py-1 md:py-3 w-[200px] px-3 font-medium"
+            />
+          </div>
+          <div className="flex xl:w-[75%]">
+            <p className="w-[80%] text-xl lg:text-2xl p-2 ">Tranås</p>
+            <BookButton
+              text="BOKA HÄR"
+              buttonStyle="text-sm md:text-xl py-1 md:py-3 w-[200px] px-3 font-medium"
+            />
+          </div>
+        </div> */}
+    </section>
+  );
+};
 
-        </section>
-    )
-}
-
-export default PriceMap
+export default PriceMap;
