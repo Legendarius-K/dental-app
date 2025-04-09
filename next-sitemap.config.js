8084f8b2cc6cf85d8f1f24d884e29a8315cac0b6
darius <darius.kaya@gmail.com>
2025-04-09 08:45:54 +0200

    sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.tryggtandvard.se/",
+  generateRobotsTxt: true, // Det här genererar även en robots.txt-fil
+  // Andra alternativ finns att konfigurera om du vill
+};
